a9199b49428ac93c0d3b0d321b4cc30612a53162
Pedro Pena <pedrohenrique.pena@outlook.com>
2023-03-03 20:56:02 -0300

    excluindo agenda.py

--- exercicios/agenda.py
+++
@@ -1,9 +0,0 @@
-"""
-Crie uma classe Agenda que pode armezenar 10 pessoas e seja capazes de realizar as seguintes operações: 
-- armazena pessoa
-- remove pessoa
-- busca pessoa
-- imprimi agenda
-- imprime pessao
-
-"""
--- modelo.py
+++ modelo.py
@@ -16,9 +16,9 @@ class Programa:
     def nome(self):
         return self._nome
 
-    #@nome.setter
-    #def nome(self, novo_nome):
-        #self._nome = novo_nome.title()
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
 
 
     def  __str__ (self):
